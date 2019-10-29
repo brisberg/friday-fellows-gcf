6fa6eac357bd3c60c2d82ca1088dc010c269d1ff
brisberg <brandon.risberg@gmail.com>
2019-10-28 19:15:43 -0700

    Added a glob to the index.ts file which will gather and export all cloud functions for export

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,1 +1,24 @@
-export {syncFromVotingSheet} from './syncFromVotingSheet';
+/**
+ * EXPORT ALL FUNCTIONS
+ *
+ *   Loads all `.f.js` files
+ *   Exports a cloud function matching the file name
+ *   Author: David King
+ *   Edited: Tarik Huber
+ *   Based on this thread:
+ *     https://github.com/firebase/functions-samples/issues/170
+ */
+import camelCase from 'camelcase';
+import {sync} from 'glob';
+
+const files =
+    sync('./**/*.f.js', {cwd: __dirname, ignore: './node_modules/**'});
+for (let f = 0, fl = files.length; f < fl; f++) {
+  const file = files[f];
+  const functionName =
+      camelCase(file.slice(0, -5).split('/').join('_'));  // Strip off '.f.js'
+  if (!process.env.FUNCTION_NAME ||
+      process.env.FUNCTION_NAME === functionName) {
+    exports[functionName] = require(file);
+  }
+}
--- functions/src/syncFromVotingSheet.f.ts
+++ functions/src/syncFromVotingSheet.f.ts
@@ -3,7 +3,7 @@ import * as functions from 'firebase-functions';
 import {SCOPES, SPREADSHEET_ID} from './config'
 import {getSheetsClient} from './google.auth';
 
-export const syncFromVotingSheet = functions.https.onRequest(async (_, res) => {
+exports = module.exports = functions.https.onRequest(async (_, res) => {
   const api = await getSheetsClient(SCOPES);
   const request = api.spreadsheets.get({spreadsheetId: SPREADSHEET_ID});
   try {
