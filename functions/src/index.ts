1f4150b1a7e3f6fd9cf36bdfe222d8661ed0bf16
brisberg <brandon.risberg@gmail.com>
2019-10-28 17:10:02 -0700

    Published first cloud function to read sheets using the .getClient service account auth option

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,26 @@
 import * as functions from 'firebase-functions';
+import {google} from 'googleapis';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+export const syncFromVotingSheet = functions.https.onRequest(
+    (_, res) => {google.auth
+                     .getClient({
+                       scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+                     })
+                     .then((auth: any) => {
+                       const api = google.sheets({version: 'v4', auth});
+                       const request = api.spreadsheets.get({
+                         spreadsheetId:
+                             '1uKWMRmtN5R0Lf3iNMVmwenZCNeDntGRK7is6Jl8wi6M'
+                       });
+                       return request;
+                     })
+                     // This just prints out all Worksheet names as an example
+                     .then(({data: {sheets}}: any) => {
+                       res.status(200).send({sheets});
+                     })
+                     .catch((err: any) => {
+                       res.status(500).send({err});
+                     })});
