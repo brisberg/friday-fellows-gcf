cb466032bb1647284b647cdf8bb2f51e349b36b3
brisberg <brandon.risberg@gmail.com>
2019-11-06 10:01:17 -0800

    setSeasonStartDate now queries for existing metadata and creats/updates depending if it exists or not

--- functions/src/setSeasonStartDate.f.ts
+++ functions/src/setSeasonStartDate.f.ts
@@ -1,12 +1,19 @@
 import * as functions from 'firebase-functions';
+import {sheets_v4} from 'googleapis';
 
 import {SCOPES, SPREADSHEET_ID} from './config'
 import {getSheetsClient} from './google.auth';
 import {START_DATE_METADATA_KEY} from './model/fridayfellows';
 
+// query for the metadata, if found update it. If not create a  new metadata for
+// the value.
+
+
 // Testing (Made spreadsheet publicly editable for the moment)
 // TODO: Figure out service account editing permissions
-// curl -X POST -H 'Content-Type: application/json' -d '{"sheetId": 1242888778, "startDate": 5432}' https://us-central1-driven-utility-202807.cloudfunctions.net/setSeasonStartDate
+// curl -X POST -H 'Content-Type: application/json' -d '{"sheetId": 1242888778,
+// "startDate": 5432}'
+// https://us-central1-driven-utility-202807.cloudfunctions.net/setSeasonStartDate
 
 exports = module.exports = functions.https.onRequest(async (req, res) => {
   const {sheetId, startDate} = req.body;
@@ -16,32 +23,72 @@ exports = module.exports = functions.https.onRequest(async (req, res) => {
 
   const api = await getSheetsClient(SCOPES);
 
-  // Look up metadata for the sheet by the key
-  // Update all values in place (including the new start date)
-  // This will create the metadata if none is found
+  const lookupReq = api.spreadsheets.developerMetadata.search({
+    spreadsheetId: SPREADSHEET_ID,
+    requestBody: {
+      dataFilters: [{
+        developerMetadataLookup: {
+          metadataKey: START_DATE_METADATA_KEY,
+          metadataLocation: {
+            sheetId,
+          }
+        }
+      }],
+    },
+  });
+
+  const existingMetadata = await lookupReq;
+
+  let metadataId;
+  const matchedMetadata = existingMetadata.data.matchedDeveloperMetadata;
+  if (matchedMetadata && matchedMetadata.length > 0) {
+    metadataId = matchedMetadata[0].developerMetadata!.metadataId;
+  }
+
+  const requests: sheets_v4.Schema$Request[] = [];
+  if (metadataId) {
+    // Metadata exists, update it
+    requests.push({
+      updateDeveloperMetadata: {
+        dataFilters: [{
+          developerMetadataLookup: {
+            metadataKey: START_DATE_METADATA_KEY,
+          },
+        }],
+        developerMetadata: {
+          metadataKey: START_DATE_METADATA_KEY,
+          metadataValue: '' + startDate,
+          location: {
+            sheetId,
+          },
+          visibility: 'PROJECT',
+        },
+        fields: 'metadataValue',
+      },
+    });
+  } else {
+    // No metadata exists, need to create it
+    requests.push({
+      createDeveloperMetadata: {
+        developerMetadata: {
+          metadataKey: START_DATE_METADATA_KEY,
+          metadataValue: '' + startDate,
+          location: {
+            sheetId,
+          },
+          visibility: 'PROJECT',
+        }
+      }
+    });
+  }
+
   const request = api.spreadsheets.batchUpdate({
     spreadsheetId: SPREADSHEET_ID,
     requestBody: {
-      requests: [{
-        updateDeveloperMetadata: {
-          dataFilters: [{
-            developerMetadataLookup: {
-              metadataKey: START_DATE_METADATA_KEY,
-            },
-          }],
-          developerMetadata: {
-            metadataKey: START_DATE_METADATA_KEY,
-            metadataValue: '' + startDate,
-            location: {
-              sheetId,
-            },
-            visibility: 'PROJECT',
-          },
-          fields: '*',
-        },
-      }],
+      requests,
     },
-  })
+  });
+
   try {
     const resp = await request;
 
