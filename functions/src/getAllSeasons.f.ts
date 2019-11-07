3770be686eb7df9c44853c454a045540760e6755
brisberg <brandon.risberg@gmail.com>
2019-11-06 17:57:04 -0800

    Added a simple cloud function to query all of the seasons

---
+++ functions/src/getAllSeasons.f.ts
@@ -0,0 +1,21 @@
+import {Firestore} from '@google-cloud/firestore';
+import * as functions from 'firebase-functions';
+
+import {PROJECT_ID} from './config'
+import {SEASONS_COLLECTION} from './model/fridayfellows';
+
+const firestore = new Firestore({
+  projectId: PROJECT_ID,
+});
+
+/**
+ * Query Firestore for the list of all seasons.
+ */
+exports = module.exports = functions.https.onRequest(async (_, res) => {
+  try {
+    const snapshot = await firestore.collection(SEASONS_COLLECTION).get();
+    res.status(200).send(snapshot.docs.map((docSnap) => docSnap.data()));
+  } catch (err) {
+    res.status(err.status).send({err});
+  }
+});
