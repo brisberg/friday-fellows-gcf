f47055ae6910d20fd17172e20e9d80a7078b8360
brisberg <brandon.risberg@gmail.com>
2019-11-12 21:25:42 -0800

    Split out a SeasonList component for the main list view

--- app/src/app/App.tsx
+++ app/src/app/App.tsx
@@ -3,28 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 import { reducer, initialState, SeasonModel } from '../state/reducer';
 import { AppActions } from '../state/actions';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
-import DateFnsUtils from '@date-io/date-fns';
-import { Icon, Tooltip } from '@material-ui/core';
 import axios from 'axios';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
-
-const useStyles = makeStyles({
-  root: {
-    overflowX: 'auto',
-    margin: '0 10px',
-  },
-  table: {
-    minWidth: 650,
-  },
-});
+import { BrowserRouter as Router } from 'react-router-dom';
+import SeasonList from '../season-list/SeasonList'
 
 interface AppProps {
   backendURI: string;
@@ -33,8 +14,6 @@ interface AppProps {
 const App: React.FC<AppProps> = (props) => {
   const { backendURI } = props;
   const [state, dispatch] = useReducer(reducer, initialState);
-
-  const classes = useStyles();
 
   const fetchSeasonData = async () => {
     const resp = await fetch(backendURI + '/getAllSeasons')
@@ -67,52 +46,7 @@ const App: React.FC<AppProps> = (props) => {
         <div className="App-body">
           <div>
             <p>Seasons JSON:</p>
-            <Paper className={classes.root}>
-              <Table className={classes.table} aria-label="simple table">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Season Name</TableCell>
-                    <TableCell align="right">Season</TableCell>
-                    <TableCell align="right">Year</TableCell>
-                    <TableCell align="right">Start&nbsp;Date&nbsp;(ms)</TableCell>
-                    <TableCell align="right">Sheet&nbsp;ID</TableCell>
-                  </TableRow>
-                </TableHead>
-                {/* TODO Add an empty state message and a loading spinner */}
-                <TableBody>
-                  {state.seasons.map((season, index) => (
-                    <TableRow key={season.sheetId}>
-                      <TableCell component="th" scope="row">
-                        <Link to={'/s/' + season.sheetId}>
-                          {season.formattedName}
-                        </Link>
-                      </TableCell>
-                      <TableCell align="right">{season.season}</TableCell>
-                      <TableCell align="right">{season.year}</TableCell>
-                      <TableCell align="right">
-                        {season.startDate === null && <Tooltip title="Missing Start Date">
-                          <Icon className="push-right warning-icon text-top">warning</Icon>
-                        </Tooltip>}
-                        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                          <KeyboardDatePicker
-                            disableToolbar
-                            variant="inline"
-                            format="MM/dd/yyyy"
-                            autoOk={true}
-                            value={season.startDate}
-                            onChange={(date) => { handleStartDateChanged(date, season, index); }}
-                            KeyboardButtonProps={{
-                              'aria-label': 'change date',
-                            }}
-                          />
-                        </MuiPickersUtilsProvider>
-                      </TableCell>
-                      <TableCell align="right">{season.sheetId}</TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </Paper>
+            <SeasonList seasons={state.seasons} onStartDateChanged={handleStartDateChanged} />
           </div>
         </div>
         <div className="App-footer">
---
+++ app/src/season-list/SeasonList.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import './SeasonList.css';
+import { SeasonModel } from '../state/reducer';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
+import { Icon, Tooltip } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+
+const useStyles = makeStyles({
+  root: {
+    overflowX: 'auto',
+    margin: '0 10px',
+  },
+  table: {
+    minWidth: 650,
+  },
+});
+
+interface SeasonListProps {
+  seasons: SeasonModel[];
+  onStartDateChanged: (date: Date | null, season: SeasonModel, index: number) => void;
+}
+
+const SeasonList: React.FC<SeasonListProps> = ({ seasons, onStartDateChanged }) => {
+
+  const classes = useStyles({});
+
+  return (
+    <Paper className={classes.root}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Season Name</TableCell>
+            <TableCell align="right">Season</TableCell>
+            <TableCell align="right">Year</TableCell>
+            <TableCell align="right">Start&nbsp;Date&nbsp;(ms)</TableCell>
+            <TableCell align="right">Sheet&nbsp;ID</TableCell>
+          </TableRow>
+        </TableHead>
+        {/* TODO Add an empty state message and a loading spinner */}
+        <TableBody>
+          {seasons.map((season, index) => (
+            <TableRow key={season.sheetId}>
+              <TableCell component="th" scope="row">
+                <Link to={'/s/' + season.sheetId}>
+                  {season.formattedName}
+                </Link>
+              </TableCell>
+              <TableCell align="right">{season.season}</TableCell>
+              <TableCell align="right">{season.year}</TableCell>
+              <TableCell align="right">
+                {season.startDate === null && <Tooltip title="Missing Start Date">
+                  <Icon className="push-right warning-icon text-top">warning</Icon>
+                </Tooltip>}
+                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                  <KeyboardDatePicker
+                    disableToolbar
+                    variant="inline"
+                    format="MM/dd/yyyy"
+                    autoOk={true}
+                    value={season.startDate}
+                    onChange={(date) => { onStartDateChanged(date, season, index); }}
+                    KeyboardButtonProps={{
+                      'aria-label': 'change date',
+                    }}
+                  />
+                </MuiPickersUtilsProvider>
+              </TableCell>
+              <TableCell align="right">{season.sheetId}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
+  );
+}
+
+export default SeasonList;
