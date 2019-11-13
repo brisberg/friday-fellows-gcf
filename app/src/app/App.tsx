eb1fd69a3b813572cb033836d7ffbf672cc26618
brisberg <brandon.risberg@gmail.com>
2019-11-12 20:18:24 -0800

    Added simple router links on season rows to change URL to each season

--- app/src/app/App.tsx
+++ app/src/app/App.tsx
@@ -14,6 +14,7 @@ import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/picker
 import DateFnsUtils from '@date-io/date-fns';
 import { Icon, Tooltip } from '@material-ui/core';
 import axios from 'axios';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 
 const useStyles = makeStyles({
   root: {
@@ -55,66 +56,70 @@ const App: React.FC<AppProps> = (props) => {
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Friday Fellows Updater - Season List
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Friday Fellows Updater - Season List
         </p>
-      </header>
-      <div className="App-body">
-        <div>
-          <p>Seasons JSON:</p>
-          <Paper className={classes.root}>
-            <Table className={classes.table} aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell>Season Name</TableCell>
-                  <TableCell align="right">Season</TableCell>
-                  <TableCell align="right">Year</TableCell>
-                  <TableCell align="right">Start&nbsp;Date&nbsp;(ms)</TableCell>
-                  <TableCell align="right">Sheet&nbsp;ID</TableCell>
-                </TableRow>
-              </TableHead>
-              {/* TODO Add an empty state message and a loading spinner */}
-              <TableBody>
-                {state.seasons.map((season, index) => (
-                  <TableRow key={season.sheetId}>
-                    <TableCell component="th" scope="row">
-                      {season.formattedName}
-                    </TableCell>
-                    <TableCell align="right">{season.season}</TableCell>
-                    <TableCell align="right">{season.year}</TableCell>
-                    <TableCell align="right">
-                      {season.startDate === null && <Tooltip title="Missing Start Date">
-                        <Icon className="push-right warning-icon text-top">warning</Icon>
-                      </Tooltip>}
-                      <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                        <KeyboardDatePicker
-                          disableToolbar
-                          variant="inline"
-                          format="MM/dd/yyyy"
-                          autoOk={true}
-                          value={season.startDate}
-                          onChange={(date) => { handleStartDateChanged(date, season, index); }}
-                          KeyboardButtonProps={{
-                            'aria-label': 'change date',
-                          }}
-                        />
-                      </MuiPickersUtilsProvider>
-                    </TableCell>
-                    <TableCell align="right">{season.sheetId}</TableCell>
+        </header>
+        <div className="App-body">
+          <div>
+            <p>Seasons JSON:</p>
+            <Paper className={classes.root}>
+              <Table className={classes.table} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Season Name</TableCell>
+                    <TableCell align="right">Season</TableCell>
+                    <TableCell align="right">Year</TableCell>
+                    <TableCell align="right">Start&nbsp;Date&nbsp;(ms)</TableCell>
+                    <TableCell align="right">Sheet&nbsp;ID</TableCell>
                   </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </Paper>
+                </TableHead>
+                {/* TODO Add an empty state message and a loading spinner */}
+                <TableBody>
+                  {state.seasons.map((season, index) => (
+                    <TableRow key={season.sheetId}>
+                      <TableCell component="th" scope="row">
+                        <Link to={'/s/' + season.sheetId}>
+                          {season.formattedName}
+                        </Link>
+                      </TableCell>
+                      <TableCell align="right">{season.season}</TableCell>
+                      <TableCell align="right">{season.year}</TableCell>
+                      <TableCell align="right">
+                        {season.startDate === null && <Tooltip title="Missing Start Date">
+                          <Icon className="push-right warning-icon text-top">warning</Icon>
+                        </Tooltip>}
+                        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                          <KeyboardDatePicker
+                            disableToolbar
+                            variant="inline"
+                            format="MM/dd/yyyy"
+                            autoOk={true}
+                            value={season.startDate}
+                            onChange={(date) => { handleStartDateChanged(date, season, index); }}
+                            KeyboardButtonProps={{
+                              'aria-label': 'change date',
+                            }}
+                          />
+                        </MuiPickersUtilsProvider>
+                      </TableCell>
+                      <TableCell align="right">{season.sheetId}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </Paper>
+          </div>
+        </div>
+        <div className="App-footer">
+          <button onClick={fetchSeasonData}>Fetch Season Data</button>
         </div>
       </div>
-      <div className="App-footer">
-        <button onClick={fetchSeasonData}>Fetch Season Data</button>
-      </div>
-    </div>
+    </Router>
   );
 }
 
