84069228ca6541122af1bd7087e4b52e290a349c
brisberg <brandon.risberg@gmail.com>
2019-11-13 21:50:13 -0800

    Converted App Header to Material UI AppHeader component

--- app/src/app/App.tsx
+++ app/src/app/App.tsx
@@ -4,13 +4,56 @@ import './App.css';
 import { reducer, initialState, SeasonModel } from '../state/reducer';
 import { AppActions } from '../state/actions';
 import axios from 'axios';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
 import SeasonList from './SeasonList';
 import SeasonDetail from './SeasonDetail';
 import OnDeck from './OnDeck';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 interface AppProps {
   backendURI: string;
+}
+
+const useStyles = makeStyles((theme: Theme) => {
+  return createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    logo: {
+      marginRight: theme.spacing(2),
+    },
+    grow: {
+      flexGrow: 1,
+    },
+  });
+});
+
+function AppHeader() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.logo} color="inherit" aria-label="logo">
+            <Link to={'/'}>
+              <img src={logo} className="App-logo" alt="logo" />
+            </Link>
+          </IconButton>
+          <Typography variant="h6">
+            Friday Fellows Updater
+          </Typography>
+          <div className={classes.grow} />
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
 const App: React.FC<AppProps> = ({ backendURI }) => {
@@ -32,15 +75,6 @@ const App: React.FC<AppProps> = ({ backendURI }) => {
         startDate: newDate,
       }))
     }
-  }
-
-  function AppHeader() {
-    return (
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Friday Fellows Updater - Season List</p>
-      </header>
-    );
   }
 
   function AppFooter() {
