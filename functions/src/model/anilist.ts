80c727463fecb8abed7a85adbc67badd6491ea4b
brisberg <brandon.risberg@gmail.com>
2019-11-15 16:10:04 -0800

    Extracted the (currently unused) Anilist model definitions into their own file

---
+++ functions/src/model/anilist.ts
@@ -0,0 +1,18 @@
+import {SeriesModel} from './fridayfellows';
+
+/// Export to List Service models
+export enum WatchStatus {
+  Watching = 0,
+  Dropped,
+  Completed,
+}
+
+export interface AnimeListRecord {
+  series: SeriesModel;
+  status: WatchStatus;
+  startDate: Date;
+  completedDate?: Date;
+  tags: string[];
+  score?: number;
+  progress: number;
+}
