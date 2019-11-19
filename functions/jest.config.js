0f41414d941021b06cee704cc422185c5875108d
brisberg <brandon.risberg@gmail.com>
2019-11-18 18:17:27 -0800

    Moved jest config into a js file so we can have comments

---
+++ functions/jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  transform: {'^.+\\.tsx?$': 'ts-jest'},
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  // github.com/kulshekhar/ts-jest/issues/259#issuecomment-504088010
+  maxWorkers: 1,  // speeds up tests
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+}
