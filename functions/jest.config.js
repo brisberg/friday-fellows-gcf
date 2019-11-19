4c41f88c52cfeba6a8bd076ce749c2c542679800
brisberg <brandon.risberg@gmail.com>
2019-11-18 19:03:00 -0800

    Jest ignoring the built js files

--- functions/jest.config.js
+++ functions/jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   transform: {'^.+\\.tsx?$': 'ts-jest'},
+  testPathIgnorePatterns: ['lib/'],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
   // github.com/kulshekhar/ts-jest/issues/259#issuecomment-504088010
   maxWorkers: 1,  // speeds up tests
