607956a54746654520e0b37f227a5b1a15776328
brisberg <brandon.risberg@gmail.com>
2019-12-01 21:47:39 -0800

    Updated googleapi mock for developermetadata and added 'jest-mocks' to deps

--- functions/src/__mocks__/googleapis.ts
+++ functions/src/__mocks__/googleapis.ts
@@ -1,14 +1,31 @@
-import {mockResponse} from '../helpers/testing/mockSpreadsheetResponse';
+import {sheets_v4} from 'googleapis';
+import {fn} from 'jest-mock';
+
+import {mockSetStartDateMetadataResponse} from '../helpers/testing/mockSetStartDateResponse';
+import {mockSpreadsheetGetResponse} from '../helpers/testing/mockSpreadsheetResponse';
+
+const sheetsAPI = {
+  spreadsheets: {
+    get: fn(() => Promise.resolve({data: mockSpreadsheetGetResponse})),
+    developerMetadata: {
+      search:
+          fn(() => Promise.resolve<
+                   {data: sheets_v4.Schema$SearchDeveloperMetadataResponse}>({
+            data: {
+              matchedDeveloperMetadata: [],
+            },
+          })),
+    },
+    batchUpdate:
+        fn(() => Promise.resolve({data: mockSetStartDateMetadataResponse})),
+  },
+};
 
 export const google = {
   auth: {
-    getClient: jest.fn(() => Promise.resolve({auth: {}})),
+    getClient: fn(() => Promise.resolve({auth: {}})),
   },
   sheets: () => {
-    return {
-      spreadsheets: {
-        get: jest.fn(() => Promise.resolve({data: mockResponse})),
-      },
-    };
+    return sheetsAPI;
   },
 };
