9e41736eaba33fe14bdb711bb21977ba005db3a0
brisberg <brandon.risberg@gmail.com>
2019-12-03 00:07:29 -0800

    Added a test and query for anilist, currently using prod anilist for the test

--- functions/src/setSeriesId.f.ts
+++ functions/src/setSeriesId.f.ts
@@ -27,13 +27,49 @@ export const setSeriesId = functions.https.onRequest((req, res) => {
       return;
     }
 
+    // Here we define our query as a multi-line string
+    // Storing it in a separate .graphql/.gql file is also possible
+    const query = `
+query ($id: Int) {
+  Media (id: $id, type: ANIME) {
+    title {
+      romaji
+      english
+      native
+    }
+    id
+    idMal
+    format
+    episodes
+    season
+    seasonInt
+  }
+}
+`;
+
+    // Define our query variables and values that will be used in the query
+    // request
+    const variables = {id: seriesId};
+
+    // Define the config we'll need for our Api request
+    const url = 'https://graphql.anilist.co', options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify({query: query, variables: variables})
+    };
+
     try {
+      // Make the HTTP Api request
+      const data = await fetch(url, options).then(resp => resp.json());
       const payload: SetSeriesIdResponse = {
-        data: req.body,
+        data: data,
       };
       res.status(200).send(payload);
     } catch (err) {
-      res.status(err.status).send({err});
+      res.status(500).send({err});
     }
   });
 });
