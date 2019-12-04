9a4ff5545e330fbc0080aed01d57ced04bc63a85
brisberg <brandon.risberg@gmail.com>
2019-12-04 01:00:26 -0800

    Storing all the relevent data in a JSON string in the metadata value instead of just the ALID

--- model/sheets.ts
+++ model/sheets.ts
@@ -26,6 +26,15 @@ export interface WorksheetModel {
  * Metadata key for the Anilist.co Id for the series associated with this row.
  */
 export const SERIES_AL_ID_KEY = 'series-anilist-id';
+/** Payload to stringify as the value for the seriesID metadata */
+export interface SeriesMetadataPayload {
+  alId: number;
+  malId: number;
+  type: string;
+  episodes: number;
+  titleEn: string;
+}
+
 /**
  * Metadata key for the type ('SERIES', 'SHORT') of the series associated with
  * this row.
