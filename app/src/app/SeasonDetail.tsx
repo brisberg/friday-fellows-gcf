619fc0d5cdf96e1fb4f65b18bb0c5bf985923cbc
brisberg <brandon.risberg@gmail.com>
2019-12-25 16:32:29 -0800

    Added help icon button for anilist links

--- app/src/app/SeasonDetail.tsx
+++ app/src/app/SeasonDetail.tsx
@@ -7,6 +7,8 @@ import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/picker
 import DateFnsUtils from '@date-io/date-fns';
 import { Tooltip, Icon, Paper, Table, TableHead, TableRow, TableCell, TableBody, Dialog, DialogTitle, Button, DialogActions, DialogContent, TextField } from '@material-ui/core';
 import ToggleButton from '@material-ui/lab/ToggleButton';
+import IconButton from '@material-ui/core/IconButton';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
 import { SeasonModel, SeriesModel } from '../../../model/firestore';
 import { AppActions } from '../state/actions';
 import { GetAllSeriesResponse } from '../../../model/service';
@@ -41,6 +43,9 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     datePicker: {
       margin: 0,
+    },
+    pushLeft: {
+      marginLeft: '10px',
     },
     paperBody: {
       margin: 'auto',
@@ -236,9 +241,10 @@ function SeriesVotingGrid({ seriesList }: { seriesList: SeriesModel[] }) {
           {seriesList.map((series) => (
             <TableRow key={series.rowIndex}>
               <TableCell component="th" scope="row">
-                <Button className="no-uppercase" href={`https://anilist.co/anime/${series.idAL}`} target="_blank">
-                  {series.titleRaw}
-                </Button>
+                {series.titleRaw}
+                {series.idAL !== -1 && <IconButton className={classes.pushLeft} href={`https://anilist.co/anime/${series.idAL}`} target="_blank">
+                  <HelpOutlineIcon fontSize="small" />
+                </IconButton>}
               </TableCell>
               <TableCell>{VotingStatus[series.votingStatus]}</TableCell>
               {series.votingRecord.map((record) => (
