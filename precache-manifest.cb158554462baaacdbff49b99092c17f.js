be46d0cc26134764f6d6d7849ed4982c67ad74f3
brisberg <brandon.risberg@gmail.com>
2020-11-04 09:12:34 -0800

    Updates

---
+++ precache-manifest.cb158554462baaacdbff49b99092c17f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddc754a9008c9683f87f982b957b11c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "638f145ce6732be9ad81",
+    "url": "/static/css/main.37dea8a7.chunk.css"
+  },
+  {
+    "revision": "4b6ba003ef41c6135e20",
+    "url": "/static/js/2.fcf6311b.chunk.js"
+  },
+  {
+    "revision": "c55518790613e0ab3983dee5d720b79d",
+    "url": "/static/js/2.fcf6311b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "638f145ce6732be9ad81",
+    "url": "/static/js/main.d2b43032.chunk.js"
+  },
+  {
+    "revision": "4cfbb4dcc8f22db11d4b",
+    "url": "/static/js/runtime-main.e65194fe.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/friday-fellows-gcf/precache-manifest.4861f0bb9a679be64410fc7e45dd5e3f.js"
+  "/precache-manifest.cb158554462baaacdbff49b99092c17f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/friday-fellows-gcf/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
